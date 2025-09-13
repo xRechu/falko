db6add8e3861a5b5564c49b52123912736114004
xRechu <jakub.reszka@yahoo.com>
2025-09-13 13:15:58 +0200

    fix(loyalty-plugin): add package.json and index shim under backend/plugins/loyalty for Medusa plugin resolution

---
+++ backend/plugins/loyalty/index.ts
@@ -0,0 +1,4 @@
+// Thin re-export layer pointing to src/plugins/loyalty
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const init = require('../../src/plugins/loyalty').default
+export default init
